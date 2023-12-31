66d6ab2227add96f15e13cb33abef12a36f8f85f
kenlevine <1@>
2023-12-30 22:38:56 -0500

    sleep v2

--- user/sleep.c
+++ user/sleep.c
@@ -1,24 +1,23 @@
 #include "kernel/types.h"
-#include "user/user.h" //该头文件对kernel/types.h头文件有类型依赖，故要放在kernel/types.h之后。
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("no argument\n");
-    } else if (argc > 2) {
-        printf("Excessive argument\n");
-    } else {
+#include "user/users.h"
+int main(int argc, char *argv[]){
+    if(argc<2){
+        printf("[Empty Argument.\n]");
+    }else if(argc>2){
+        printf("[Excessive Arguement]\n");
+    }else{
         int tag = 1;
-        char *p = argv[1];
-        while (*p) {
-            if (*p < '0' || *p > '9') {
+        int *p = argv[1];
+        while(*p){
+            if(*p<'0' || *p>'9'){
                 tag = 0;
                 break;
             }
             p++;
         }
-        if (tag) {
+        if(tag){
             sleep(atoi(argv[1]));
-        } else {
+        }else {
             printf("Illegal argument\n");
         }
     }
