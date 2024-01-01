670612ee6cf838ca4fbdb32edb5c20d511d0eb84
kenlevine <1@>
2024-01-01 10:44:21 -0500

    pingpong finished

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,14 +4,17 @@
 int main(){
     int p[2];
     pipe(p);
-    if(fork()){
+    pid = fork();
+    if(pid){
         // if(fork()) is equal to if(fork()>0)
         write(p[1],"ping\n",6);
-        printf("[Successfully Write]\n");
+        wait(0);
+        printf("[Successfully Write]\n",getpid());
     }else{
         // else is equal to else if(fork()==0)
          read(p[0],"pong\n",6);
-         printf("[Successfully Read]\n");
+         wait(0);
+         printf("[Successfully Read]\n",pid);
     }
 
     exit(0);
