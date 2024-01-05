0168d1736c04e989d45b95b7990cc60c19e79d18
kenlevine <1@>
2024-01-04 23:19:15 -0500

    test

--- user/xargs.c
+++ user/xargs.c
@@ -21,12 +21,14 @@ int main(int argc, char* argv[]){
             args[i - 1] = argv[i]; // 拷贝其他参数
         }
 
-        // 执行命令
-        int tag = exec(argv[1], args);
-        if(tag == -1){
-            printf("执行错误\n");
-            exit(1);
-        }
+        exec("echo", args);
+
+        // // 执行命令
+        // int tag = exec(argv[1], args);
+        // if(tag == -1){
+        //     printf("执行错误\n");
+        //     exit(1);
+        // }
     } else {
         printf("参数不足，无法执行命令。\n");
     }
