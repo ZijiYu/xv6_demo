c5022eeaeb4afea7a8371dce7d18697606382fd6
kenlevine <1@>
2024-01-04 21:41:34 -0500

    init xargs

---
+++ user/xargs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+# define MSGSIZE 16
+
+int main(int argc, char* argv[]){
+    char buf[MSGSIZE];
+    read(0, buf, MSGSIZE);
+    printf("进程读取到的内容：%s",buf);
+
+    for(int i = 0; i < argc, i++){
+        printf("argv[%d]: %s",i,argv[i]);
+    }
+}
