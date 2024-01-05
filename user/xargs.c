83b5709d0a5786bddde3994ffe7b3d5794d6e310
kenlevine <1@>
2024-01-04 21:53:48 -0500

    debug

--- user/xargs.c
+++ user/xargs.c
@@ -6,10 +6,10 @@
 
 int main(int argc, char* argv[]){
     char buf[MSGSIZE];
-    read(0, buf, MSGSIZE);
+    int index = read(0, buf, MSGSIZE);
     printf("进程读取到的内容：%s",buf);
 
-    for(int i = 0; i < argc; i++;){
+    for(int i = 0; i < argc; i++){
         printf("argv[%d]: %s",i,argv[i]);
     }
     exit(0);
