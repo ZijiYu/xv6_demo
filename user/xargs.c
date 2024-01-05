350197fde80150fdb573e20f0584cf4bf542e48c
kenlevine <1@>
2024-01-04 22:06:38 -0500

    xargs add exec

--- user/xargs.c
+++ user/xargs.c
@@ -7,10 +7,19 @@
 int main(int argc, char* argv[]){
     char buf[MSGSIZE];
     read(0, buf, MSGSIZE);
-    printf("进程读取到的内容：%s",buf);
+    
+    // printf("进程读取到的内容：%s",buf);
 
-    for(int i = 0; i < argc; i++){
-        printf("argv[%d]: %s \n",i,argv[i]);
+    // for(int i = 0; i < argc; i++){
+    //     printf("argv[%d]: %s \n",i,argv[i]);
+    // }
+
+    // echo bye | echo hello to
+
+
+    for(int i = 2; i < argc; i++){
+        exec(argv[1],argv[i]);
     }
+    exec(argv[1], buf);
     exit(0);
 }
