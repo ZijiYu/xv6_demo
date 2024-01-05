7f6245284f911670d10105217a31744934593952
kenlevine <1@>
2024-01-05 18:46:21 -0500

    test

--- user/xargs.c
+++ user/xargs.c
@@ -7,7 +7,7 @@
 int main(int argc, char* argv[]){
     //获取前一个命令的标准化输出（即此命令标准化输入）
 
-    sleep(100);
+    sleep(30);
     char buf[MSGSIZE];
     read(0, buf, MSGSIZE);
 
@@ -27,12 +27,11 @@ int main(int argc, char* argv[]){
             xargv[xargc] = p;
             xargc++;
             xargv[xargc] = 0;
-            // if(fork()){
-            //     wait(0);
-            // }else{
-            //     exec(argv[1],xargv);
-            // }
-            exec(argv[1],xargv);
+            if(fork()){
+                wait(0);
+            }else{
+                exec(argv[1],xargv);
+            }
         }
     }
     exit(0);
