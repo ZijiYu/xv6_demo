bf907529550db58a7c11b48c66d2cb370fe08d10
kenlevine <1@>
2024-01-13 19:47:04 -0500

    test

--- kernel/proc.c
+++ kernel/proc.c
@@ -662,8 +662,8 @@ procdump(void)
 uint64
 get_used_proc(){
   struct proc *p = myproc();
+
   uint64 n = 0;
-  
   for(p = proc; p < &proc[NPROC]; p++){
     acquire(&p->lock);
     if(p->state != UNUSED){
--- user/user.h
+++ user/user.h
@@ -26,6 +26,15 @@ int sleep(int);
 int uptime(void);
 int trace(int);
 int sysinfo(struct sysinfo*);
+#ifdef LAB_NET
+int connect(uint32, uint16, uint16);
+#endif
+#ifdef LAB_PGTBL
+int pgaccess(void *base, int len, void *mask);
+// usyscall region
+int ugetpid(void);
+#endif
+
 
 // ulib.c
 int stat(const char*, struct stat*);
