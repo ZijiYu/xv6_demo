a82b4c2178266ad8de6eda64cef85f996d6fed05
kenlevine <1@>
2024-01-19 11:42:16 -0500

    test

--- user/test.c
+++ user/test.c
@@ -4,13 +4,23 @@
 #include <stddef.h>
 
 void main(){
-int pid = fork();
-if (!pid) {
-    printf("this is child\n"); // this line
-} else{
-    printf("parent: I have a child=%d\n", pid);// this line
-    wait(NULL);
-    printf("child status: %d \n", pid);
-}
-exit(0);
+    int pid = fork();
+    if (pid > 0) {
+        // 父进程
+        printf("parent: I have a child=%d\n", pid);
+        // 父进程先执行所需操作
+        // ... 父进程的代码 ...
+
+        // 等待子进程结束
+        wait(NULL);
+        printf("child %d is done\n", pid);
+    } else if (pid == 0) {
+        // 子进程
+        // ... 子进程的代码 ...
+        printf("this is child\n");
+    } else {
+        // fork失败
+        printf("fork error\n");
+    }
+    exit(0);
 }
